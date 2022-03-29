91d27c0b458d96bfdb3906b3d3875200c6cf3005
Zahir-seid <54268715+Zahir-seid@users.noreply.github.com>
2022-03-29 21:55:24 +0300

    personal data

--- bash.sh
+++
@@ -1,7 +0,0 @@
-git clone https://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@github.com/Zahir-seid/alx_zero_day.git
-cd alx_zero_day
-echo 'my first Readme' > README.md
-git config --global user.email 'zahirseid101@gmail.com'
-git config --global user.name 'Zahir-seid'
-git add .
-git  commit -m 'my first commit'
